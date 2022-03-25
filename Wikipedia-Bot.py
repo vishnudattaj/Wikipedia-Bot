9a52bafc6fd171d5bac76a961f16cc81140bce69
vishnudattaj <101271583+vishnudattaj@users.noreply.github.com>
2022-03-24 19:35:26 -0500

    Update Wikipedia-Bot.py

--- Wikipedia-Bot.py
+++ Wikipedia-Bot.py
@@ -1,5 +1,6 @@
 import discord
 import wikipediaapi
+import urbandict
 
 TOKEN = "OTUwODU2MzIxNTEzNjg1MDAy.Yie_9Q.JuBcAZWDmgDC9JTBFZmzz5L6FTQ"
 
@@ -17,6 +18,12 @@ async def on_message(message):
     user_message = str(message.content)
     channel = str(message.channel.name)
     print(f'{username}: {user_message} ({channel})')
+    if message.content.startswith('!urban')
+        if message.author == client.user:
+                return
+    else:
+        urban_input = str(message.content).split('-')[1]
+        urbandict.define(urban_input)
     if message.content.startswith('!help'):
         await message.channel.send(
             'Welcome to Wikipedia-Bot! This bot searches user-inputted material into wikipedia, and shares the result to you. The format is !wiki-input_in_wikipedia')
