be8369938ed3876f76bbb5cb3b99316375ad9d14
vishnudattaj <101271583+vishnudattaj@users.noreply.github.com>
2022-03-26 13:31:49 -0500

    Update Wikipedia-Bot.py

--- Wikipedia-Bot.py
+++ Wikipedia-Bot.py
@@ -2,7 +2,6 @@ import discord
 import wikipediaapi
 from udpy import UrbanClient
 
-
 TOKEN = "OTUwODU2MzIxNTEzNjg1MDAy.Yie_9Q.J8-PG2g9J5oos7N0O2Vtd7-y8QY"
 
 client = discord.Client()
@@ -23,20 +22,21 @@ async def on_message(message):
         u = 0
         urban_input = str(message.content).split('-')[1]
         urban = UrbanClient()
-        defs = urban.get_definition(urban_input)
-        for d in defs:
+        for d in urban.get_definition(urban_input):
             u += 1
             if u == 1:
-                embedVar = discord.Embed(title=d.word, color=0x00ffff)
-                embedVar.add_field(name="Definition:", value=d.definition, inline=False)
-                embedVar.add_field(name="Example:", value=d.example, inline=False)
-                embedVar.add_field(name="Upvotes:", value=str(d.upvotes) + str('\U0001F44D'), inline=False)
-                embedVar.add_field(name="Downvotes:", value=str(d.downvotes) + str('\U0001F44E'), inline=False)
-                await message.channel.send(embed=embedVar)
+                embed_var = discord.Embed(title=d.word, color=0x00ffff)
+                embed_var.add_field(name="Definition:", value=d.definition, inline=False)
+                embed_var.add_field(name="Example:", value=d.example, inline=False)
+                embed_var.add_field(name="Upvotes:", value=str(d.upvotes) + str('\U0001F44D'), inline=False)
+                embed_var.add_field(name="Downvotes:", value=str(d.downvotes) + str('\U0001F44E'), inline=False)
+                await message.channel.send(embed=embed_var)
 
     if message.content.startswith('!help'):
         await message.channel.send(
-            'Welcome to Wikipedia-Bot! This bot searches user-inputted material into wikipedia, and shares the result to you. The format is !wiki-input_in_wikipedia! It also accesses urban disctionary. To access urban dictionary, the format is !urban-input_in_urban_dictionary')
+            'Welcome to Wikipedia-Bot! This bot searches user-inputted material into wikipedia, and shares the result to you. '
+            'The format is !wiki-input_in_wikipedia! It also accesses urban dictionary. '
+            'To access urban dictionary, the format is !urban-input_in_urban_dictionary')
     if message.content.startswith('!wiki'):
         if message.author == client.user:
             return
@@ -48,8 +48,8 @@ async def on_message(message):
             if not page_py.exists():
                 await message.channel.send("Error: Page not found")
             else:
-                embedVar = discord.Embed(title=page_py.title, url=page_py.fullurl, description=str(page_py.summary[0:1024]) + '...', color=0x00ffff)
-                await message.channel.send(embed=embedVar)
+                embed_var1 = discord.Embed(title=page_py.title, url=page_py.fullurl, description=str(page_py.summary[0:1024]) + '...', color=0x00ffff)
+                await message.channel.send(embed=embed_var1)
 
 
 @client.event
@@ -63,6 +63,3 @@ async def on_message_delete(message):
 
 
 client.run(TOKEN)
-
-
-
